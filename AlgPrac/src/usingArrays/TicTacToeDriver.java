2e353b6615d1910ae5a0d1874f053e9e2052151e
benjamin lin <xianlin@cs.stonybrook.edu>
2015-03-19 20:35:00 -0400

    usingArrays: 2-dim array - TicTacToe game

---
+++ AlgPrac/src/usingArrays/TicTacToeDriver.java
@@ -0,0 +1,24 @@
+package usingArrays;
+
+public class TicTacToeDriver {
+	
+	public static void main(String[] args) {
+		TicTacToe ttt = new TicTacToe();
+		
+		/* X moves */				/* O moves */
+		ttt.putMark(1, 1);			ttt.putMark(0, 2);
+		ttt.putMark(2, 2); 			ttt.putMark(0, 0);
+		ttt.putMark(0, 1); 			ttt.putMark(2, 1);
+		ttt.putMark(1, 2); 			ttt.putMark(1, 0);
+		ttt.putMark(2, 0);
+		
+		System.out.println(ttt.toString());
+		int winningPlayer = ttt.winner();
+		if (winningPlayer != 0) {
+			System.out.println(winningPlayer + " Wins");
+		} else {
+			System.out.println("Tie");
+		}
+	}
+	
+}
