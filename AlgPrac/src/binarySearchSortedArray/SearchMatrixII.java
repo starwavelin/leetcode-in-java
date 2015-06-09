9a91963a9a6e4d8d9b660a8d89501eb618ec7597
Benjamin X. Lin <benjaminxianlin@gmail.com>
2015-06-09 16:10:20 -0400

    binarySearchSortedArray: SearchMatrixII done.

---
+++ AlgPrac/src/binarySearchSortedArray/SearchMatrixII.java
@@ -0,0 +1,73 @@
+package binarySearchSortedArray;
+
+import java.util.Scanner;
+/**
+ * 
+ * Write an efficient algorithm that searches for a value in an m x n matrix, return the occurrence of it.
+ * This matrix has the following properties:
+    * Integers in each row are sorted from left to right.
+    * Integers in each column are sorted from up to bottom.
+    * No duplicate integers in each row or column.
+ *
+ */
+
+public class SearchMatrixII {
+	
+	/**
+     * @param matrix: A list of lists of integers
+     * @param: A number you want to search in the matrix
+     * @return: An integer indicate the occurrence of target in the given matrix
+     */
+    public static int searchMatrix(int[][] matrix, int target) {
+        // Idea: From the lower left corner, move either to the right or up to find target
+    	// O(n + m)
+    	int count = 0;
+    	if (matrix == null || matrix.length == 0 || matrix[0][0] > target) {
+    		return count;
+    	}
+    	
+    	int numRows = matrix.length;
+    	int numCols = matrix[0].length;
+    	int row = numRows - 1;
+    	int col = 0;
+    	for (int i = 0; i < numRows + numCols - 1; i++) {
+    		if (matrix[row][col] == target) {
+    			count++;
+    			if (col + 1 == numCols) {
+    				return count;
+    			}
+    			col++; // move to the right
+    		} else if (matrix[row][col] > target) {
+    			if (row == 0) {
+    				return count;
+    			}
+    			row--; // move up
+    		} else {
+    			if (col + 1 == numCols) {
+    				return count;
+    			}
+    			col++;
+    		}
+    	}
+    	return count;
+    }
+	
+	public static void main(String[] args) {
+		System.out.println("*** Welcome to Ben's Search Matrix II Test ***");
+		
+		int[][] matrix = new int[][] {
+				{0, 1, 2, 4},
+				{1, 2, 5, 7},
+				{3, 5, 7, 10},
+				{7, 8, 9, 11}
+		};
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Give your target number: ");
+		int target = sc.nextInt();
+
+		int ret = searchMatrix(matrix, target);
+		System.out.println("The target number " + target + 
+					" occurred in the matrix " + ret + " time(s)");
+	}
+}
