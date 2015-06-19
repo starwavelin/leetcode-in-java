5fb96dee248f199122209c98ffa0a58d7bb9723b
Benjamin X. Lin <benjaminxianlin@gmail.com>
2015-06-19 09:50:55 -0400

    string: implement atoi

---
+++ AlgPrac/src/string/AToI.java
@@ -0,0 +1,49 @@
+package string;
+
+import java.util.Scanner;
+
+/**
+ * Convert a String into an Integer
+ * 
+ */
+public class AToI {
+	
+	// Integer.MAX_VALUE = 2147483647  the last digit of this number is 7
+	
+	private static final int maxDiv10 = Integer.MAX_VALUE / 10;
+	public static int atoi(String str) {
+		int i = 0;
+		str = str.trim();
+		int n = str.length();
+		int sign = 1; // sign = 1 represents "+"
+		if (i < n && str.charAt(i) == '+') {
+			i++;
+		} else if (i < n && str.charAt(i) == '-') {
+			sign = -1;
+			i++;
+		}
+		int num = 0;
+		while (i < n && Character.isDigit(str.charAt(i))) {
+			int digit = Character.getNumericValue(str.charAt(i));
+			
+			// overflow handling
+			if (num > maxDiv10 || num == maxDiv10 && digit >= 8) {
+				return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+			}
+			num = num * 10 + digit;
+			i++;
+		}
+		return sign * num;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("*** Welcome to Ben's String to Integer Test ***");
+		
+		System.out.print("Give a String that you wanna convert to int type: ");
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		
+		System.out.println("Your input as an integer should be " + atoi(s));
+	}
+	
+}
