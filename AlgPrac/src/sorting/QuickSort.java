3c378790f670d974dc3fe85ef98a67c8ea50b9c7
starwavelin <benjaminxianlin@gmail.com>
2017-07-31 20:51:45 -0400

    quicksort One with partiton function extracted out

--- AlgPrac/src/sorting/QuickSort.java
+++ AlgPrac/src/sorting/QuickSort.java
@@ -3,8 +3,7 @@ package sorting;
 import java.util.Scanner;
 
 /**
- * This is my QuickSort version I using the middle element of the input array as
- * the pivot
+ * This is my QuickSort Version One using pivot as the middle element of the input array
  */
 public class QuickSort {
 
@@ -19,52 +18,49 @@ public class QuickSort {
 	private static void sort(int[] nums, int low, int high) {
 		if (high <= low) {
 			return;
-		} else {
-			int i = low, j = high;
-			
-			// Get the pivot element from the middle of the list
-			int pivot = nums[low + (high - low) / 2];
-
-			// Divide into two lists
-			while (i <= j) {
-				// If the current value from the left list is smaller than the
-				// pivot element, then get the next element from the left list
-				while (nums[i] < pivot) {
-					i++;
-				}
-				// If the current value from the right list is larger than the
-				// pivot element, then get the next element from the right list
-				while (nums[j] > pivot) {
-					j--;
-				}
-
-				// If we have found a value in the left list larger
-				// than or equal the pivot element and if we have found a value in the right
-				// list smaller than the pivot element,
-				// then we exchange the two values.
-				// As we are done we can increase i and j
-				if (i <= j) {  // from <= to <, infinity loop
-					swap(nums, i, j);
-					i++;
-					j--;
-				}
-			}
-			// Recursion
-			if (low < j)
-				sort(nums, low, j);
-			if (i < high)
-				sort(nums, i, high);
 		}
+		// Get the pivot element from the middle of the list
+		int pivot = nums[low + (high - low) / 2];
+		int pivotIndex = partition(nums, low, high, pivot);
+		// Recursion
+		sort(nums, low, pivotIndex -1);
+		sort(nums, pivotIndex + 1, high);
 	}
 
-	private static void swap(int[] nums, int i, int j) {
-		int temp = nums[i];
-		nums[i] = nums[j];
-		nums[j] = temp;
+	private static int partition(int[] nums, int low, int high, int pivot) {
+		int i = low, j = high;
+		
+		// Divide into two lists
+		while (i <= j) {
+			// If the current value from the left list is smaller than the
+			// pivot element, then get the next element from the left list
+			while (nums[i] < pivot) { //if change to <=, stackoverflow, why?
+				i++;
+			}
+			// If the current value from the right list is larger than the
+			// pivot element, then get the next element from the right list
+			while (nums[j] > pivot) {
+				j--;
+			}
+
+			// If we have found a value in the left list larger
+			// than or equal the pivot element and if we have found a value in the right
+			// list smaller than the pivot element,
+			// then we exchange the two values.
+			// As we are done we can increase i and j
+			if (i <= j) {  // from <= to <, infinity loop
+				int temp = nums[i];
+				nums[i] = nums[j];
+				nums[j] = temp;
+				i++;
+				j--;
+			}
+		}
+		return i;
 	}
 
 	public static void main(String[] args) {
-		System.out.println("*** Welcome to Ben's Quick Sort I Test ***");
+		System.out.println("*** Welcome to Ben's Quick Sort One (pivot as middle element) Test ***");
 
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Input your integer array, \n"
