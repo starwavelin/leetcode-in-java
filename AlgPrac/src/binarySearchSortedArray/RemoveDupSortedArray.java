2f41a5c4768f6282656e7fc8dc4223e0e9e43347
starwavelin <benjaminxianlin@gmail.com>
2017-10-21 16:44:47 -0400

    Remove Dups from sorted array I revisited

--- AlgPrac/src/binarySearchSortedArray/RemoveDupSortedArray.java
+++ AlgPrac/src/binarySearchSortedArray/RemoveDupSortedArray.java
@@ -1,24 +1,48 @@
 package binarySearchSortedArray;
 
 import java.util.Scanner;
-/**
- * Remove duplicates in a sorted array such that each element 
- * appear only once and return the new length.
- * Do not allocate extra space for another array, 
- * you must do this in place with constant memory.
- * http://www.lintcode.com/en/problem/remove-duplicates-from-sorted-array/
- */
+
+/***************************************************************************
+* Problem No. : 26
+* Problem Name: Remove Duplicates from Sorted Array
+* Problem URL : https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+* Date        : Oct 21 2017
+* Author      :	Xian Lin
+* Notes       : 
+* 	Scenario: 
+* 		Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+* 	Assumption:
+* 		1. Sorted Array
+* 		2. Do it in place
+	Example:
+* 	Input: [1, 2, 2]
+* 	Output: should return new length 2, and the first two elements in the new array will be 1, 2, respectively.
+* 	Data Structure and Alg:
+* 		Use two pointers prev and cur, prev starts from index 0 and cur starts from index 1
+* 		Use cur to traverse the array till cur reaches the array length
+* 			if (nums[prev] == nums[cur]) cur++ to continue traverse the array
+* 			if (nums[prev] != nums[cur]) prev++, use nums[cur] to replace nums[prev], cur++
+* 		The length of the new array if prev + 1.
+* 		
+* Complexity  : 
+* 	Time Complexity: O(n) cuz traverse the whole array
+* 	Space Complexity: O(1) cuz just open two pointers cur and prev
+* 
+* meta        : tag-array, tag-two-pointers
+***************************************************************************/
 public class RemoveDupSortedArray {
 	
 	public static int removeDup(int[] nums) {
-		if (nums.length < 2) {
+		if (nums == null || nums.length < 2) {
 			return nums.length;
 		}
-		int prev = 0;
-		for (int cur = 0; cur < nums.length; cur++) {
-			if (nums[prev] != nums[cur]) {
+		int prev = 0, cur = 1;
+		while (cur < nums.length) {
+			if (nums[prev] == nums[cur])
+				cur++;
+			else {
 				prev++;
-				nums[prev] = nums[cur];
+				nums[prev] = nums[cur++];
 			}
 		}
 		return prev + 1;
@@ -32,19 +56,17 @@ public class RemoveDupSortedArray {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("*** Welcome to Ben's Remove Duplicates in Sorted Array Test ***");
-		
+		System.out.println("*** Welcome to Xian's Remove Duplicates in Sorted Array I Test ***");
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Input your integer sorted array (with duplicates), \n" +
-				"leave each number by space: ");
+		System.out.print("Input your integer sorted array (with duplicates), leave each number by space: ");
 		String[] strs = sc.nextLine().split(" ");
 		int[] testArray = new int[strs.length];
 		for (int i = 0; i < strs.length; i++) {
 			testArray[i] = Integer.parseInt(strs[i]);
 		}
-		
 		int size = removeDup(testArray);
-		if (size > 0) {
+		System.out.println(String.format("The new array has size: %d", size));
+		if (removeDup(testArray) > 0) {
 			displayArray(testArray, size);
 		} else {
 			System.out.print("Check your input array!!");
