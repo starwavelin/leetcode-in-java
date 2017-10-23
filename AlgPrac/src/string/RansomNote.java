f91102bb7e6eb4442f815081e056c39d68793a35
starwavelin <benjaminxianlin@gmail.com>
2017-10-23 14:41:45 -0400

    Solution 2 and 3 added for Ransom Note

--- AlgPrac/src/string/RansomNote.java
+++ AlgPrac/src/string/RansomNote.java
@@ -63,11 +63,72 @@ public class RansomNote {
         return true;
     }
 
+	/**
+	 * Solution 2:
+	 * Idea is similar to Sol 1 but instead of using a high resource consumed hashmap, 
+	 * just use an array map of size 256, assuming at most ASCII extension code are used for input strings 
+	 * In this array map, index is the ASCII number of a character and value is its frequency
+	 * Time Complexity: O(n)
+	 * Space Complexity: O(1)/O(n) depending on the length of magazine string vs 256 
+	 */
+	public static boolean canConstruct2(String ransomNote, String magazine) {
+		if (ransomNote == null || ransomNote == "" 
+        		|| magazine == null || magazine == "") {
+        	return false;
+        }
+		int[] map = new int[256];
+		char[] mag = magazine.toCharArray();
+		char[] ran = ransomNote.toCharArray();
+		for (char c : mag) {
+			map[c]++;
+		}
+		for (char c : ran) {
+			map[c]--;
+			if (map[c] < 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * Solution 3:
+	 * Idea is similar to Sol 2, 
+	 * but just use an array map of size 26, based on the assumption that only lowercase Latin characters are used 
+	 * In this array map, index 0 is for 'a' and 25 is for 'z', and the value is a char's frequency
+	 * Time Complexity: O(n)
+	 * Space Complexity: O(1)/O(n) depending on the length of magazine string vs 26 
+	 */
+	public static boolean canConstruct3(String ransomNote, String magazine) {
+		if (ransomNote == null || ransomNote == "" 
+        		|| magazine == null || magazine == "") {
+        	return false;
+        }
+		int[] map = new int[26]; //26 lowercase Latin characters
+		char[] mag = magazine.toCharArray();
+		char[] ran = ransomNote.toCharArray();
+		for (char c : mag) {
+			map[c - 'a']++; // storing index has a left 97 offset
+		}
+		for (char c : ran) {
+			map[c - 'a']--;
+			if (map[c - 'a'] < 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 	
 	public static void main(String[] args) {
 		String str1 = "aa", str2 = "ab", str3 = "aba";
+		/* Solution 1 */
 		System.out.println("Can str1 be constructed by str2? " + canConstruct1(str1, str2));
 		System.out.println("Can str1 be constructed by str3? " + canConstruct1(str1, str3));
-		 
+		/* Solution 2 */
+		System.out.println("Can str1 be constructed by str2? " + canConstruct2(str1, str2));
+		System.out.println("Can str1 be constructed by str3? " + canConstruct2(str1, str3));
+		/* Solution 3 */
+		System.out.println("Can str1 be constructed by str2? " + canConstruct3(str1, str2));
+		System.out.println("Can str1 be constructed by str3? " + canConstruct3(str1, str3));
 	}
 }
