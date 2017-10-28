7cb51d1cc32913e6cb52ee23ba03742718a9b905
starwavelin <benjaminxianlin@gmail.com>
2017-10-27 23:40:05 -0400

    3sum skeleton up

---
+++ AlgPrac/src/array/ThreeSum.java
@@ -0,0 +1,27 @@
+package array;
+
+/***************************************************************************
+* Problem No. : 15
+* Problem Name: Three Sum
+* Problem URL : https://leetcode.com/problems/3sum/description/
+* Date        : Oct 27 2017
+* Author	  :	Xian Lin
+* Notes       : 
+* 	Scenario: 
+* 		@needOrganize
+* 	Assumption:
+* 		1. 
+	Example:
+* 	Input: 
+* 	Output: 
+* 	Data Structure and Alg:
+* 		See Code Comments  
+* Complexity  : 
+* 	Time Complexity: O() -- See Code Comments
+* 	Space Complexity: O() -- See Code Comments
+* 
+* meta        : tag-array, tag-two-pointers, tag-sort, tag-hash
+***************************************************************************/
+public class ThreeSum {
+
+}
