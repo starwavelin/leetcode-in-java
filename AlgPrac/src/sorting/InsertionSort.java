ad680761cd23811643b49d2c88aac383c06a4abd
starwavelin <benjaminxianlin@gmail.com>
2017-11-13 17:23:04 -0500

    insertionSort revisited

--- AlgPrac/src/sorting/InsertionSort.java
+++ AlgPrac/src/sorting/InsertionSort.java
@@ -15,34 +15,18 @@ import java.util.Scanner;
 
 public class InsertionSort {
 
-	public static void insertionSort(int[] input) {		
-		for (int i = 1; i < input.length; i++) {
-			int cur = input[i];
+	public static void insertionSort(int[] nums) {		
+		for (int i = 1; i < nums.length; i++) {
+			int cur = nums[i];
 			int j = i - 1;
-			while ((j >= 0) && (input[j] > cur)) {
-				input[j + 1] = input[j];
+			while (j >= 0 && nums[j] > cur) {
+				nums[j + 1] = nums[j];
 				j--;
 			}
-			if (j + 1 !=i) {	// add this if condition to optimize performance
-				input[j + 1] = cur;
+			if (j + 1 != i) {	// add this if condition to optimize performance
+				nums[j + 1] = cur;
 			}
 		}
-	}
-	
-	public static void main(String[] args) {
-		System.out.println("*** Welcome to Ben's Insertion Sort Test ***");
-
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Input your integer array, \n"
-				+ "leave each number by space: ");
-		String[] strs = sc.nextLine().split(" ");
-		int[] testArray = new int[strs.length];
-		for (int i = 0; i < strs.length; i++) {
-			testArray[i] = Integer.parseInt(strs[i]);
-		}
-		
-		insertionSort(testArray);
-		displayResult(testArray);
 	}
 	
 	public static void displayResult(int[] ret) {
@@ -50,5 +34,18 @@ public class InsertionSort {
 			System.out.print(element + " ");
 		}
 		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("*** Welcome to Xian's Insertion Sort Test ***");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Input your integer array, leave each number by space: ");
+		String[] strs = sc.nextLine().split(" ");
+		int[] testArray = new int[strs.length];
+		for (int i = 0; i < strs.length; i++) {
+			testArray[i] = Integer.parseInt(strs[i]);
+		}
+		insertionSort(testArray);
+		displayResult(testArray);
 	}
 }
