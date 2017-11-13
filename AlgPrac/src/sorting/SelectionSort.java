d5bd76f19cd7beae49ee1ec8aeb9d393b6471bd6
starwavelin <benjaminxianlin@gmail.com>
2017-11-13 16:18:20 -0500

    selection sort revisited

--- AlgPrac/src/sorting/SelectionSort.java
+++ AlgPrac/src/sorting/SelectionSort.java
@@ -15,40 +15,20 @@ import java.util.Scanner;
 
 public class SelectionSort {
 
-	public static void selectionSort(int[] input) {		
-		for (int i = 0; i < input.length - 1; i++) {
-			int minIndex = i;
-			for (int j = i + 1; j < input.length; j++) {
-				if (input[j] < input[minIndex]) {
-					minIndex = j;
+	public static void selectionSort(int[] nums) {		
+		for (int i = 0; i < nums.length - 1; i++) {
+			int min = i;
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[j] < nums[min]) {
+					min = j;
 				}
 			}
-			if (i != minIndex) {
-				swap(i, minIndex, input);
+			if (i != min) {
+				int tmp = nums[i];
+				nums[i] = nums[min];
+				nums[min] = tmp;
 			}
 		}
-	}
-	
-	private static void swap(int i, int j, int[] input) {
-		int tmp = input[i];
-		input[i] = input[j];
-		input[j] = tmp;
-	}
-	
-	public static void main(String[] args) {
-		System.out.println("*** Welcome to Ben's Selection Sort Test ***");
-
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Input your integer array, \n"
-				+ "leave each number by space: ");
-		String[] strs = sc.nextLine().split(" ");
-		int[] testArray = new int[strs.length];
-		for (int i = 0; i < strs.length; i++) {
-			testArray[i] = Integer.parseInt(strs[i]);
-		}
-		
-		selectionSort(testArray);
-		displayResult(testArray);
 	}
 	
 	public static void displayResult(int[] ret) {
@@ -56,5 +36,18 @@ public class SelectionSort {
 			System.out.print(element + " ");
 		}
 		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("*** Welcome to Xian's Selection Sort Test ***");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Input your integer array, leave each number by space: ");
+		String[] strs = sc.nextLine().split(" ");
+		int[] testArray = new int[strs.length];
+		for (int i = 0; i < strs.length; i++) {
+			testArray[i] = Integer.parseInt(strs[i]);
+		}
+		selectionSort(testArray);
+		displayResult(testArray);
 	}
 }
