1372bbface4404751e239d89de099e0b37947bcc
starwavelin <benjaminxianlin@gmail.com>
2017-11-14 12:02:56 -0500

    MergeSortedArray OpenNewSpace solution AC

--- AlgPrac/src/array/MergeSortedArray.java
+++ AlgPrac/src/array/MergeSortedArray.java
@@ -125,15 +125,18 @@ public class MergeSortedArray {
 		}
 		int[] tmp = new int[m + n];
 		int i = 0, j = 0, k = 0;
-		while (k < m + n) {
-			if (i < m && j < n) {
-				tmp[k++] = (nums1[i] < nums2[j]) ? nums1[i++] : nums2[j++];
-			} else if (i >= m && j < n) {
-				tmp[k++] = nums2[j++];
-			} else if (i < m && j >= n) {
-				tmp[k++] = nums1[i++];
-			}
+		
+		/* 归并排序 mergeArray 部分代码块 */
+		while (i < m && j < n) {
+			tmp[k++] = (nums1[i] < nums2[j]) ? nums1[i++] : nums2[j++];
 		}
+		while (i < m) {
+			tmp[k++] = nums1[i++];
+		}
+		while (j < n) {
+			tmp[k++] = nums2[j++];
+		}
+		
 		return tmp;
 	}
 	
