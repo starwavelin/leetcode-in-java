140b2a79f00e815501480e48e8179dc48d964c25
starwavelin <benjaminxianlin@gmail.com>
2017-12-30 18:35:02 -0500

    Friend Circles DFS sol up

---
+++ AlgPrac/src/graph/FriendCircles.java
@@ -0,0 +1,78 @@
+package graph;
+
+/***************************************************************************
+* Problem No. : 547
+* Problem Name: Friend Circles
+* Problem URL : https://leetcode.com/problems/friend-circles/description/
+* Date        : Dec 30 2017
+* Author	  	  :	Xian Lin
+* Notes       : 
+* 	Scenario: 
+* 		Given a matrix M of n*n representing the friendship of n students in a class. 
+* 		And we define the friendship are transitive. 
+* 		Find out the number of friend circles in this class. 
+* 	Assumption:
+* 		1. N is in range [1, 200]
+* 		2. M[i][i] = 1 for all students
+* 		3. if M[i][j] = 1, then M[j][i] = 1 as well
+	Example: 
+* 	Input: 
+* 	Output: 
+* 		
+* 	Data Structure and Alg:
+* 		See Code Comments  
+* Complexity  : 
+* 	Time Complexity: O() -- See Code Comments
+* 	Space Complexity: O() -- See Code Comments
+* 
+* meta        : tag-graph
+***************************************************************************/
+public class FriendCircles {
+	/**	 
+	 * Solution 1: using DFS
+	 */
+	public static class FC1 {
+		public int findCircleNum(int[][] M) {
+			int n = M.length;
+			if (n == 0) {
+				return 0;
+			}
+			int res = 0;
+			boolean[] visited = new boolean[n];
+			for (int i = 0; i < n; i++) {
+				if (!visited[i]) {
+					dfs(M, visited, i, n);
+					res++;
+				}
+			}
+			return res;
+		}
+		
+		private void dfs(int[][] M, boolean[] visited, int i, int n) {
+			for (int j = 0; j < n; j++) {
+				if (M[i][j] == 1 && !visited[j]) {
+					visited[j] = true; //dye
+					dfs(M, visited, j, n);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Solution 2: Using Union Find
+	 */
+	public static class FC2 {
+		public int findCircleNum(int[][] M) {
+			return 0;
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		int[][] M1 = new int[][] {{1, 0, 0, 1}, {0, 1, 1, 0}, {0, 1, 1, 1}, {1, 0, 1, 1}};
+		int[][] M2 = new int[][] {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+		FC1 fc1 = new FC1();
+		System.out.println("Number of Friend Circles for M1: " + fc1.findCircleNum(M1)); //should return 1
+		System.out.println("Number of Friend Circles for M1: " + fc1.findCircleNum(M2)); //should return 2
+	}
+}
