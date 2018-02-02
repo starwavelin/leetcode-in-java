4fd53bb1179be51150ad88214155fb218b164cc8
starwavelin <benjaminxianlin@gmail.com>
2018-02-01 23:03:47 -0500

    added maxDepth and isBalanced of Binary Tree

---
+++ AlgPrac/src/binaryTree/MaxDepth.java
@@ -0,0 +1,17 @@
+package binaryTree;
+
+import utility.TreeNode;
+
+public class MaxDepth {
+	
+	public static class Solution {
+		public int maxDepth(TreeNode root) {
+	        if (root == null)
+	            return 0;
+	        if (root.left == null && root.right == null)
+	            return 1;
+	        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+	    }
+	}
+
+}
