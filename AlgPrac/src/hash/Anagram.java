c63090a9052ee2d18651659e5d8f960292175f2b
starwavelin <benjaminxianlin@gmail.com>
2018-04-06 18:33:40 -0400

    fix problem number err

--- AlgPrac/src/hash/Anagram.java
+++ AlgPrac/src/hash/Anagram.java
@@ -4,29 +4,29 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 /***************************************************************************
-* Problem No. : 241
+* Problem No. : 242
 * Problem Name: Valid Anagram
 * Problem URL : https://leetcode.com/problems/valid-anagram/description/
 * Date        : Oct 26 2017
 * Author      :	Xian Lin
-* Notes       : 
-* 	Scenario: 
+* Notes       :
+* 	Scenario:
 * 		@needOrganize
 * 	Assumption:
-* 
+*
 	Example:
 * 	Input/Output:
-* 		
+*
 * 	Data Structure and Alg:
 * 		See solutions in the code comments
-* Complexity  : 
+* Complexity  :
 * 	Time Complexity: See solutions in the code comments
-* 	Space Complexity: See solutions in the code comments   
-* 
+* 	Space Complexity: See solutions in the code comments
+*
 * meta        : tag-hash, tag-array-map, tag-bloomberg
 ***************************************************************************/
 public class Anagram {
-	
+
 	/**
 	 * This method uses API methods like Arrays.sort()
 	 * 	and toCharArray();
@@ -41,13 +41,13 @@ public class Anagram {
 		if (s.length() != t.length()) {
 			return false;
 		}
-		
+
 		char[] s1 = s.toCharArray();
 		char[] t1 = t.toCharArray();
-		
+
 		Arrays.sort(s1);
 		Arrays.sort(t1);
-		
+
 		int size = s1.length;
 		for (int i = 0; i <size; i++) {
 			if (s1[i] != t1[i]) {
@@ -56,7 +56,7 @@ public class Anagram {
 		}
 		return true;
 	}
-	
+
 	/**
 	 * Use an integer array to count the appearance of characters
 	 * in the two given strings. If there is an unmatched character,
@@ -73,7 +73,7 @@ public class Anagram {
 		if (s.length() != t.length()) {
 			return false;
 		}
-		
+
 		int[] count = new int[256];
 		int size = s.length();
 		for (int i = 0; i < size; i++) {
@@ -87,17 +87,17 @@ public class Anagram {
 		}
 		return true;
 	}
-	
-	
+
+
 	public static void main(String[] args) {
 		System.out.println("*** Welcome to Ben's Anagram Test ***");
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Input a string please: ");
 		String s = sc.next().trim();
 		System.out.print("Input another string please: ");
 		String t = sc.next().trim();
-		
+
 		boolean result = anagram2(s, t);
 		if (result) {
 			System.out.println("The given " + s + " and " + t + " are anagrams.");
