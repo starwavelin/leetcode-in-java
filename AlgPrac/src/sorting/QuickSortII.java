93c9ac607fd13371eb297443d73c33f0aaecafea
starwavelin <benjaminxianlin@gmail.com>
2019-10-20 23:03:13 -0400

    quicksort: version 1 and 2 done

--- AlgPrac/src/sorting/QuickSortII.java
+++ AlgPrac/src/sorting/QuickSortII.java
@@ -3,11 +3,11 @@ package sorting;
 import java.util.Scanner;
 
 /**
- * This is my QuickSort Version 1.
+ * This is my QuickSort Version 2.
  * The index of pivot (abbreviated as variable "pivot") is defined within the partition function.
- * The pivot is always the leftmost element in a partitioned array.
+ * The pivot is always the rightmost element in a partitioned array.
  */
-public class QuickSort {
+public class QuickSortII {
 
 	public static void sort(int[] nums) {
 		if (nums == null || nums.length == 0) {
@@ -25,19 +25,19 @@ public class QuickSort {
 	}
 
 	private static int partition(int[] nums, int left, int right) {
-		int pivot = left;  // here pivot means the index, not the element
-		int i = pivot + 1; // i is the slow Pointer
+		int pivot = right;
+		int i = left - 1; // i is the slow Pointer
 		// j is the fast Pointer
-		for (int j = i; j <= right; j++) {
+		for (int j = left; j <= pivot - 1; j++) {
 		    if (nums[j] < nums[pivot]) {
-		        swap(nums, i, j);
-		        i++;
+		    	i++;
+		    	if (i != j) {
+		    		swap(nums, i, j);
+		    	}
 		    }
 		}
-		if (pivot != i - 1) {
-		    swap(nums, pivot, i - 1);
-		}
-		return i - 1;
+		swap(nums, pivot, i + 1);
+		return i + 1;
 	}
 
 	private static void swap(int[] nums, int i, int j) {
@@ -47,7 +47,7 @@ public class QuickSort {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("*** Welcome to Coding Bro's Quick Sort One (pivot as leftmost element) Test ***");
+		System.out.println("*** Welcome to Coding Bro's Quick Sort Two (pivot as rightmost element) Test ***");
 
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Input your integer array, leave each number by space: ");
